87174bf0de851af6038980dcbd8d9f73ac6c988e
Lautaro Moyano <lautaromoyano420@gmail.com>
2021-04-24 12:31:45 -0300

    Realizado (leer Readme)

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,38 @@
 module Library where
 import PdePreludat
+type Objetivo = String
+type Poder = Number
+type Entrenamiento = Number
+type Fortaleza = Number
+type Presion = Number
+type Golpe = Objetivo -> Presion
 
-doble :: Number -> Number
-doble numero = numero + numero
+presion :: Entrenamiento -> Objetivo -> Presion
+presion horasDeEntrenamiento nombreObjetivo = poder horasDeEntrenamiento / fortaleza nombreObjetivo
+
+poder :: Entrenamiento -> Poder
+poder horasDeEntrenamiento = (*15) horasDeEntrenamiento 
+
+fortaleza :: Objetivo -> Fortaleza
+fortaleza = (*2).length
+
+gomuGomu :: Golpe
+gomuGomu = presion 180
+
+golpesNormalesConsecutivos :: Golpe
+golpesNormalesConsecutivos = presion 240
+
+esmenorA :: Number -> Number -> Bool
+esmenorA cota numero = (<cota) numero
+
+objetivoEsDificil :: Objetivo -> Bool
+objetivoEsDificil = (esmenorA 100).gomuGomu
+
+estaEntre :: Number -> Number -> Number -> Bool
+estaEntre inferior superior numero = (inferior < numero) && (numero < superior)
+
+focalizarObjetivo :: Objetivo -> Objetivo
+focalizarObjetivo = take 7
+
+objetivoEsAccesible :: Objetivo -> Bool
+objetivoEsAccesible = (estaEntre 200 400).golpesNormalesConsecutivos.focalizarObjetivo
--- src/Spec.hs
+++ src/Spec.hs
@@ -5,7 +5,32 @@ import Test.Hspec
 
 correrTests :: IO ()
 correrTests = hspec $ do
-  describe "Test de ejemplo" $ do
-    it "El pdepreludat se instaló correctamente" $ do
-      doble 1 `shouldBe` 2
+  describe "Presion" $ do
+    it "presion" $ do
+       presion 200 "bolsa de entrenamiento" `shouldBe` 68.181818182
+  describe "Esta entre y menorA" $ do
+    it "200 esta entre 150 y 250" $ do
+       estaEntre 150 250 200 `shouldBe` True
+    it "0.57721 no esta entre 2.72 y 3.14" $ do
+       estaEntre 2.72 3.14 0.57721 `shouldBe` False  
+    it "500 es menor a 900" $ do
+       esmenorA 900 500 `shouldBe` True
+    it "3 no es menor a 2.72" $ do
+       esmenorA 2.72 3 `shouldBe` False
+  describe "Golpes" $ do
+    it "gomu gomu funca" $ do
+        gomuGomu "bolsa de entrenamiento" `shouldBe` 61.363636364
+    it "golpes normales consecutivos funca" $ do
+        golpesNormalesConsecutivos "bolsa de entrenamiento" `shouldBe` 81.818181818
+  describe "Objetivo es dificil funca?" $ do
+    it "Bolsa de entrenamiento es dificil" $ do
+      objetivoEsDificil "bolsa de entrenamiento"  `shouldBe` True
+    it "Puf es facil" $ do
+      objetivoEsDificil "puf" `shouldBe` False
+  describe "Objetivo es accesible funca?" $do
+    it "bolsa de entrenamiento es accesible" $ do
+      objetivoEsAccesible "bolsa de entrenamiento" `shouldBe` True
+    it "puf no es accesible" $ do
+      objetivoEsAccesible "puf" `shouldBe` False
+    
 
